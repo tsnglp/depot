8b472774a4d44da4b980b28190971e083a9a94da
Tsengel Purevdorj <tsnglp@gmail.com>
2019-07-02 13:27:25 +1000

    Add quantity to a cart items

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,11 +12,11 @@ class Product < ApplicationRecord
   before_destroy :ensure_not_referenced_by_any_line_item
 
   private
-  # ensure that there are not line items referencing this product
-  def ensure_not_referenced_by_any_line_item
-    unless line_items.empty?
-      errors.add( :base, 'Line Items present')
-      throw :abort
+    # ensure that there are not line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add( :base, 'Line Items present')
+        throw :abort
+      end
     end
-  end
 end
